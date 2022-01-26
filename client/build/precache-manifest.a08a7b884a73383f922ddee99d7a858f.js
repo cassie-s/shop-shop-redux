4eaed074c1b948bac376cd58ec3304f4fcecfa2b
cassie-s <56046933+cassie-s@users.noreply.github.com>
2022-01-25 20:14:04 -0600

    starter

---
+++ client/build/precache-manifest.a08a7b884a73383f922ddee99d7a858f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8526e389154525d175d05fb8b18d354a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc1107b05d8506429c7b",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "8fa04fea9cc7887ac9a3",
+    "url": "/static/js/2.bdf8104d.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.bdf8104d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc1107b05d8506429c7b",
+    "url": "/static/js/main.ddda0ace.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
